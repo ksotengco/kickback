840e17bf987fb4f1e08f5d0687b2ed0a11795bd7
Paul <pauldavis95@hotmail.com>
2018-11-08 22:08:14 -0600

    Being creating HomePage fragment.

--- app/src/main/java/edu/cs371m/kickback/page/HomePage.java
+++ app/src/main/java/edu/cs371m/kickback/page/HomePage.java
@@ -1,4 +1,38 @@
 package edu.cs371m.kickback.page;
 
-public class HomePage {
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+
+import edu.cs371m.kickback.R;
+
+public class HomePage extends Fragment {
+
+    private Button hostButton;
+    private Button attendButton;
+    private Button eventsButton;
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View v = inflater.inflate(R.layout.home_page, container, false);
+
+        hostButton = v.findViewById(R.id.hostShortcut);
+        hostButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+
+        attendButton = v.findViewById(R.id.attendShortcut);
+        eventsButton = v.findViewById(R.id.myEventsShortcut);
+
+        return v;
+    }
 }
