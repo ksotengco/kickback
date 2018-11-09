37956b39fae98013004b6d2d3709388955ee35a6
Paul <pauldavis95@hotmail.com>
2018-11-08 22:06:28 -0600

    Create Profile data model.

--- app/src/main/java/edu/cs371m/kickback/model/Profile.java
+++ app/src/main/java/edu/cs371m/kickback/model/Profile.java
@@ -1,4 +1,20 @@
 package edu.cs371m.kickback.model;
 
+import java.util.ArrayList;
+
 public class Profile {
+    private String firstName;
+    private String lastName;
+    private String id;
+    private String email;
+    private Photo profilePicture;
+    private ArrayList<String> hosting;
+    private ArrayList<String> attending;
+    private ArrayList<String> invites;
+    private int totalRating;
+    private int reviewCount;
+
+    public Profile(String id) {
+        // query DB for certain user information
+    }
 }
