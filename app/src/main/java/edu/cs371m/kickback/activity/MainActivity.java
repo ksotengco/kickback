b89c9e5dbe96c1f29783766aa7681ffcd3c9c5e0
Paul <pauldavis95@hotmail.com>
2018-11-28 14:12:03 -0600

    onMessagereceived log.

--- app/src/main/java/edu/cs371m/kickback/activity/MainActivity.java
+++ app/src/main/java/edu/cs371m/kickback/activity/MainActivity.java
@@ -15,6 +15,7 @@ import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.GoogleAuthCredential;
 import com.google.firebase.iid.FirebaseInstanceId;
 import com.google.firebase.iid.InstanceIdResult;
 import com.google.firebase.messaging.FirebaseMessaging;
--- app/src/main/java/edu/cs371m/kickback/service/MessagingService.java
+++ app/src/main/java/edu/cs371m/kickback/service/MessagingService.java
@@ -3,8 +3,14 @@ package edu.cs371m.kickback.service;
 import android.util.Log;
 
 import com.google.firebase.messaging.FirebaseMessagingService;
+import com.google.firebase.messaging.RemoteMessage;
 
 public class MessagingService extends FirebaseMessagingService {
+
+    @Override
+    public void onMessageReceived(RemoteMessage remoteMessage) {
+        Log.d("Message", remoteMessage.getData().get("data"));
+    }
 
     @Override
     public void onNewToken(String s) {
