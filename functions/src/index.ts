b35a5a6e2f6590d6b7025c75a772a5aa3b188bb6
Paul <pauldavis95@hotmail.com>
2018-11-28 17:32:24 -0600

    Messages seem to be working.

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -5,7 +5,12 @@ const functions = require('firebase-functions');
 
 // The Firebase Admin SDK to access the Firebase Realtime Database.
 const admin = require('firebase-admin');
-admin.initializeApp();
+const serviceAccount = require('../service.json');
+
+admin.initializeApp({
+	credential: admin.credential.cert(serviceAccount),
+  	databaseURL: 'https://kickback-7517d.firebaseio.com'
+});
 
 // Start writing Firebase Functions
 // https://firebase.google.com/docs/functions/typescript
@@ -13,5 +18,19 @@ admin.initializeApp();
 export const INVITE_NOTIFICATION = functions.firestore.document('profiles/{id}/invites/{invite}')
 	.onCreate((snapshot, context) => {
 		console.log(snapshot.data());
-		return null;
+		return admin.messaging().send({
+			data: {
+				invite: snapshot.get('eventId')
+			},
+			notification: {
+				title: 'hola',
+				body: 'hallo'
+			},
+			token: "dCZPlnDNEQY:APA91bGyTdaAne4zT89_murqGlT6LzDu84CCotG_469FeNDXLe63acwascKlQMktrpO8VKcmx38gCOY4Q9eifN2BOeg4xSbqCaMX3OVjuJwOkeAm3nPFl8fXRjYe66i8nRNnblPJyC78"
+		}).then((response) => {
+			// Response is a message ID string.
+			console.log('Successfully sent message:', response);
+		}).catch((error) => {
+			console.log('Error sending message:', error);
+		});
 	});
