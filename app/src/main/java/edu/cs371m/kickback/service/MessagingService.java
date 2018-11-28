0a7acf9be7deb4058514ee75d02a74721d49db8c
Paul <pauldavis95@hotmail.com>
2018-11-28 17:33:18 -0600

    Messages seem to be working.

--- app/src/main/java/edu/cs371m/kickback/service/MessagingService.java
+++ app/src/main/java/edu/cs371m/kickback/service/MessagingService.java
@@ -1,19 +1,44 @@
 package edu.cs371m.kickback.service;
 
+import android.support.v4.app.NotificationCompat;
 import android.util.Log;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import com.google.firebase.messaging.FirebaseMessagingService;
 import com.google.firebase.messaging.RemoteMessage;
 
+import edu.cs371m.kickback.R;
+
 public class MessagingService extends FirebaseMessagingService {
 
     @Override
+    public void onCreate() {
+        super.onCreate();
+        Log.d("messageCreate", "create");
+    }
+
+    @Override
     public void onMessageReceived(RemoteMessage remoteMessage) {
-        Log.d("Message", remoteMessage.getData().get("data"));
+        Log.d("MessageRECEIVEDa", remoteMessage.getData().get("invite"));
+        String eventID = remoteMessage.getData().get("invite");
+        Toast.makeText(this, remoteMessage.getData().get("invite"), Toast.LENGTH_SHORT).show();
+
+//        NotificationCompat.Builder mBuilder = new NotificationCompat.Builder(this, "some id")
+//                .setSmallIcon(R.drawable.ic_launcher_foreground)
+//                .setContentTitle("You have a new invite!")
+//                .setContentText(eventID)
+//                .setPriority(NotificationCompat.PRIORITY_DEFAULT);
+
     }
 
     @Override
     public void onNewToken(String s) {
         Log.d("newToken", "Token: " + s);
+    }
+
+    @Override
+    public void onDeletedMessages() {
+        super.onDeletedMessages();
     }
 }
