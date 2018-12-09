90dd945b6cd849d690119a0e42774f5631c5b65b
DESKTOP-1FEC649\Kyle-Laptop <ksotengco@gmail.com>
2018-12-09 03:12:29 -0600

    individual event pages for each event

--- app/src/main/java/edu/cs371m/kickback/page/EventPage.java
+++ app/src/main/java/edu/cs371m/kickback/page/EventPage.java
@@ -8,26 +8,43 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
 
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+
 import edu.cs371m.kickback.R;
 
-public class EventPage extends Fragment {
+public class EventPage extends Fragment implements OnMapReadyCallback {
 
     private TextView hostName;
     private TextView eventName;
     private TextView eventDesc;
 
+    private GoogleMap gmap;
+
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
         View v = inflater.inflate(R.layout.event_page, container, false);
+
+        SupportMapFragment mapFragment = (SupportMapFragment) getChildFragmentManager().findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+
         hostName = v.findViewById(R.id.host_name_info);
         eventName = v.findViewById(R.id.event_name_info);
         eventDesc = v.findViewById(R.id.event_description);
 
-        hostName.setText(getArguments().getString("host_name"));
-        eventName.setText(getArguments().getString("event_name"));
-        eventDesc.setText(getArguments().getString("event_desc"));
+        hostName.setText(getArguments().getString("hostName"));
+        eventName.setText(getArguments().getString("eventName"));
+        eventDesc.setText(getArguments().getString("description"));
 
         return v;
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        this.gmap = googleMap;
+
+
     }
 }
--- app/src/main/java/edu/cs371m/kickback/view/SearchFirestoreAdapter.java
+++ app/src/main/java/edu/cs371m/kickback/view/SearchFirestoreAdapter.java
@@ -1,6 +1,9 @@
 package edu.cs371m.kickback.view;
 
+import android.os.Bundle;
 import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.CardView;
 import android.support.v7.widget.RecyclerView;
 import android.util.Log;
 import android.view.LayoutInflater;
@@ -17,6 +20,7 @@ import java.util.TimeZone;
 
 import edu.cs371m.kickback.R;
 import edu.cs371m.kickback.model.Event;
+import edu.cs371m.kickback.page.EventPage;
 
 public class SearchFirestoreAdapter extends FirestoreRecyclerAdapter<Event, SearchFirestoreAdapter.SearchViewHolder> {
 
@@ -28,11 +32,15 @@ public class SearchFirestoreAdapter extends FirestoreRecyclerAdapter<Event, Sear
         TextView hostName;
         TextView dateView;
 
+        CardView cardView;
+
         public SearchViewHolder(View theView) {
             super(theView);
             eventName = theView.findViewById(R.id.eventName);
             hostName = theView.findViewById(R.id.eventHost);
             dateView = theView.findViewById(R.id.dateView);
+
+            cardView = theView.findViewById(R.id.event_card);
 
             // date will be shown according to user's timezone (hopefully)
             string2Date = new SimpleDateFormat("yyyy/MM/dd HH:mm:ss Z");
@@ -50,7 +58,7 @@ public class SearchFirestoreAdapter extends FirestoreRecyclerAdapter<Event, Sear
     }
 
     @Override
-    protected void onBindViewHolder(@NonNull SearchViewHolder holder, int position, @NonNull Event model) {
+    protected void onBindViewHolder(@NonNull SearchViewHolder holder, final int position, @NonNull Event model) {
         holder.eventName.setText(model.getEventName());
         holder.hostName.setText(model.getHostName());
         try {
@@ -59,14 +67,47 @@ public class SearchFirestoreAdapter extends FirestoreRecyclerAdapter<Event, Sear
             Log.d("onBindViewHolder", e.getLocalizedMessage());
             holder.dateView.setText(model.getDate());
         }
+
+        holder.cardView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Event event = getItem(position);
+                EventPage goToEvent = new EventPage();
+
+                goToEvent.setArguments(eventBundler(event));
+                ((AppCompatActivity)view.getContext()).getSupportFragmentManager()
+                        .beginTransaction()
+                        .addToBackStack(null)
+                        .replace(R.id.app_fragment, goToEvent)
+                        .commit();
+                //Toast.makeText(view.getContext(), event.getEventName(), Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 
     @NonNull
     @Override
     public SearchViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
         View v = LayoutInflater.from(parent.getContext()).inflate(R.layout.event_card, parent, false);
+
         return new SearchViewHolder(v);
     }
 
+    private Bundle eventBundler (Event event) {
+        Bundle eventInfo = new Bundle();
+
+        eventInfo.putString("eventName", event.getEventName());
+        eventInfo.putString("description", event.getDescription());
+
+        eventInfo.putString("hostId", event.getHostId());
+        eventInfo.putString("hostName", event.getHostName());
+
+        eventInfo.putString("date", event.getDate());
+
+        eventInfo.putStringArrayList("location", event.getLocation());
+        eventInfo.putStringArrayList("pending", event.getPending());
+
+        return eventInfo;
+    }
 
 }
